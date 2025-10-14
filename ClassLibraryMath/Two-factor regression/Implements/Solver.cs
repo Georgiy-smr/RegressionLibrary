419b5b368bdfd4ace3c10a4f095df04e3fff135a
Georgy <mr.smirnovgd@yandex.ru>
2025-10-15 01:01:51 +0500

    QR factorized solver

--- ClassLibraryMath/Two-factor regression/Implements/Solver.cs
+++ ClassLibraryMath/Two-factor regression/Implements/Solver.cs
@@ -1,4 +1,5 @@
-﻿using Regression.MathService;
+﻿using MathNet.Numerics.LinearAlgebra;
+using Regression.MathService;
 using Regression.Two_factor_regression.Interfaces;
 using System;
 using System.Collections.Generic;
@@ -13,4 +14,29 @@ namespace Regression.Two_factor_regression.Implements
         private readonly Gaus gaus = new Gaus();
         public IEnumerable<double> GetRoots(double[,] X, double[,] Y) => gaus.Roots(X, Y);
     }
+
+    public class SolverMathNet : ISolverSystem
+    {
+        public IEnumerable<double> GetRoots(double[,] X, double[,] Y)
+        {
+            // Создаем матрицу A из массива X
+            Matrix<double>? A = Matrix<double>.Build.DenseOfArray(X);
+
+            // Создаем вектор b из массива Y
+            // Предполагается, что Y — двумерный массив размерности m x 1
+            int rows = Y.GetLength(0);
+            int cols = Y.GetLength(1);
+            if (cols != 1)
+                throw new ArgumentException("Y должен быть вектором-столбцом (m x 1)");
+
+            Vector<double>? b = Vector<double>.Build.Dense(rows, i => Y[i, 0]);
+
+            // Решаем систему A * x = b
+            Vector<double> x = A.Solve(b);
+
+            return x;
+        }
+    }
+
+
 }
---
+++ SolversTests/GausTests.cs
@@ -0,0 +1,55 @@
+using Regression.Two_factor_regression.Implements;
+using Regression.Two_factor_regression.Interfaces;
+
+namespace SolversTests
+{
+    public class GausTests
+    {
+        [Fact]
+        public void Solve_1()
+        {
+            ISolverSystem sut = new Solver();
+
+            double[] expected = new double[2] { 1.6, 1.6 };
+
+            double[,] A = {
+                { 2, 3 },
+                { 1, -1 }
+            };
+
+            double[,] b = {
+                { 8 },
+                { 0 }
+            };
+            var roots = sut.GetRoots(A, b);
+
+            var errors = roots.Select((x, i) => x - expected[i]);
+
+            Assert.True(errors.All(x => x < 0.00001));
+        }
+
+        [Fact]
+        public void Solve_2()
+        {
+            ISolverSystem sut = new Solver();
+            
+                
+            double[] expected = new double[2] { 2.071428571428571, -0.6428571428571425 };
+
+            double[,] A = {
+                { 60, 16 },
+                { 16, 8 }
+            };
+
+            double[,] b = {
+                { 114 },
+                { 28 }
+            };
+            var roots = sut.GetRoots(A, b);
+
+            var errors = roots.Select((x, i) => x - expected[i]);
+
+            Assert.True(errors.All(x => x < 0.00001));
+        }
+    }
+}
